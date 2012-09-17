8a2c78ae77abaebc07d38113093ab36b3dcfba2b
Henrik Tunedal <tunedal@gmail.com>
2012-09-17 18:36:14 +0200

    wip

--- src/nu/tunedal/bdmem/BDMemWidget.java
+++ src/nu/tunedal/bdmem/BDMemWidget.java
@@ -1,9 +1,19 @@
 package nu.tunedal.bdmem;
 
-import android.appwidget.AppWidgetProvider;
 import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.util.Pair;
 import android.widget.RemoteViews;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import android.text.TextUtils;
+import android.provider.ContactsContract.CommonDataKinds.Event;
 
 public class BDMemWidget extends AppWidgetProvider {
     @Override
@@ -13,8 +23,79 @@ public class BDMemWidget extends AppWidgetProvider {
         for (int i=0; i < appWidgetIds.length; i++) {
             RemoteViews views = new RemoteViews(context.getPackageName(),
                                                 R.layout.bdmem_appwidget);
-            views.setTextViewText(R.id.datum1, "Tjosan!");
+            int[] dates = {R.id.datum1, R.id.datum2, R.id.datum3,
+                           R.id.datum4, R.id.datum5};
+            int[] names = {R.id.namn1, R.id.namn2, R.id.namn3,
+                           R.id.namn4, R.id.namn5};
+            int j = 0;
+            for (Pair<String,String> p : getBirthdays(context)) {
+                views.setTextViewText(dates[j], p.first);
+                views.setTextViewText(names[j], p.second);
+                if (++j == dates.length)
+                    break;
+            }
+            System.out.println("Sleeping...");
+            try {
+                Thread.sleep(30000);
+            }
+            catch (InterruptedException ex) {
+            }
+            System.out.println("Awake!");
             appWidgetManager.updateAppWidget(appWidgetIds[i], views);
         }
+    }
+
+    public List<Pair<String,String>> getBirthdays(Context context) {
+        String[] projection = {
+            ContactsContract.Data._ID,
+            Event.CONTACT_ID,
+            Event.START_DATE,
+            Event.TYPE,
+            ContactsContract.Data.MIMETYPE
+        };
+
+        ContentResolver resolver = context.getContentResolver();
+        String where = (ContactsContract.Data.MIMETYPE +
+                        " = ? AND " +
+                        ContactsContract.Data.DATA2 +
+                        " = ?");
+        Cursor cur = resolver.query(ContactsContract.Data.CONTENT_URI,
+                                    projection,
+                                    where,
+                                    new String[] {
+                                        Event.CONTENT_ITEM_TYPE,
+                                        "" + Event.TYPE_BIRTHDAY
+                                    },
+                                    ContactsContract.Data._ID + " ASC");
+        List<Pair<String,String>> list = new ArrayList<Pair<String,String>>();
+        list.add(Pair.create("Rader:", "" + cur.getCount()));
+        String[] ignorables = {"name", "phone_v2", "email_v2", "photo"};
+        Map<String,String> birthdays = new HashMap<String,String>();
+        Map<String,String> nicknames = new HashMap<String,String>();
+        while (cur.moveToNext()) {
+            birthdays.put(cur.getString(1), cur.getString(2));
+            boolean ignore = false;
+            for (String s : ignorables) {
+                if (cur.getString(projection.length - 1).endsWith("/" + s)) {
+                    ignore = true;
+                    break;
+                }
+            }
+            if (ignore) continue;
+            StringBuilder sb = new StringBuilder();
+            for (int i=0; i < projection.length; i++) {
+                sb.append(", ");
+                sb.append(cur.getString(i));
+            }
+            System.out.println(sb.toString());
+        }
+        System.out.println(TextUtils.join(", ", birthdays.keySet()));
+        // TODO: query mot kontakttabellen för att översätta id:n till namn.
+        // ...och lägg till views dynamiskt:
+        // http://stackoverflow.com/questions/4078039
+        for (Map.Entry<String,String> entry : birthdays.entrySet()) {
+            list.add(Pair.create(entry.getKey(), entry.getValue()));
+        }
+        return list;
     }
 }
